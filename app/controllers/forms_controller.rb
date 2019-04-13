028848537193b88b638839f56b442b5bbcc7f7d1
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-13 19:30:49 +0900

    params取得、createしたらnewアクションにredirect

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -5,4 +5,9 @@ class FormsController < ApplicationController
   def new
     @form = Form.new
   end
+
+  def create
+    Form.create(name: params[:form][:name], email: params[:form][:email], content: params[:form][:content])
+    redirect_to '/forms/new'
+  end
 end
