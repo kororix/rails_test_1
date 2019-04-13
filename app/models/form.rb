91dfbae140856e96c395e1c2c8e6c772c065061f
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-13 21:23:40 +0900

    140字以上の時にエラー文を出す

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,8 @@
 class Form < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { maximum: 140 }
   validates :email, presence: true
+  validates :email, length: { maximum: 140 }
   validates :content, presence: true
+  validates :content, length: { maximum: 140 }
 end
