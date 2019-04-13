755d10a2f1d7de9698fa6840708cd5bca61ec2aa
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-13 18:05:43 +0900

    inquery_formsコントローラー削除した

--- app/controllers/inquiry_forms_controller.rb
+++
@@ -1,2 +0,0 @@
-class InquiryFormsController < ApplicationController
-end
--- test/controllers/inquiry_forms_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class InquiryFormsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
