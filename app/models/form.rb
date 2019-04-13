582f346c45b8eec5ded3b96449ce8d754aec43f4
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-13 23:03:01 +0900

    init

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,8 +1,5 @@
 class Form < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { maximum: 140 }
-  validates :email, presence: true
-  validates :email, length: { maximum: 140 }
-  validates :content, presence: true
-  validates :content, length: { maximum: 140 }
+  validates :name, length: { maximum: 140 }, presence: true
+  validates :email, length: { maximum: 140 }, presence: true
+  validates :content, length: { maximum: 140 }, presence: true
 end
