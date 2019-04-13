8b9da548d6cea64568411648b8d30479cb7feefe
Ayaka Kawai <consultation.pharmacist6@gmail.com>
2019-04-13 18:00:25 +0900

    inquiry_formsコントローラーを作成

---
+++ app/controllers/inquiry_forms_controller.rb
@@ -0,0 +1,2 @@
+class InquiryFormsController < ApplicationController
+end
---
+++ test/controllers/inquiry_forms_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InquiryFormsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
